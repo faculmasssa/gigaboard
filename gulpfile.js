860eb9ce38435e1ffd4926210cd0b379c762d10c
Memz-idk <arthurdefigueiredo@hotmail.com>
2025-04-22 07:18:11 -0300

    Adicionar suporte a jsx

--- gulpfile.js
+++ gulpfile.js
@@ -13,31 +13,39 @@ const output = './public/js'
 
 // Scripts usados para cada página, 
 // Caso uma pagína use mais de um script, adiciona só o principal, NÃO ADICIONE TODOS PORFAVO 👍
-const scripts = ['cadastro', 'painel', 'admin'];
+const scripts = ['cadastro', 'painel', 'admin'].sort();
 
 if(!fs.existsSync(output)) {
     fs.mkdirSync(output);
 }
 
+let scriptsRead = [...scripts];
+let files = fs.readdirSync(input, { recursive: true }).sort().filter(f => /** @type {string} */ {
+    let arr = /** @type {string} */ (f).split('.');
+    let idx = scriptsRead.findIndex(v => v === arr[arr.length-2]);
+    if(idx !== -1) {
+        scriptsRead.splice(idx, 1);
+        return true;
+    }else {
+        return false;
+    }
+});
+
+const bundler = browserify(files.map(s => `${input}/${s}`), {debug: true})
+    .plugin(tsify)
+    .plugin(factor, { o: scripts.map(s => `${output}/${s}.js`) });
+
 // Builda uma vez
 gulp.task('build', function() {
-    return browserify(scripts.map(s => `${input}/${s}.ts`))
-        .plugin(tsify)
-        .plugin(factor, { o: scripts.map(s => `${output}/${s}.js`) })
-        .bundle()
-        .pipe(source('common.js'))
-        .pipe(gulp.dest(output));
+    return bundler.bundle().pipe(source('common.js')).pipe(gulp.dest(output));
 });
 
 // Builda várias vezes automaticamente (deixa rodando em outro terminal)
 gulp.task('watch', function() {
-    let bundler = watchify(browserify(scripts.map(s => `${input}/${s}.ts`))
-        .plugin(tsify)
-        .plugin(factor, { o: scripts.map(s => `${output}/${s}.js`) })
-    );
+    let watchifyBundler = watchify(bundler);
     function bundle() {
-        bundler.bundle().pipe(source('common.js')).pipe(gulp.dest(output));
+        watchifyBundler.bundle().pipe(source('common.js')).pipe(gulp.dest(output));
     }
     bundle();
-    bundler.on("update", bundle);
+    watchifyBundler.on('update', bundle);
 });
